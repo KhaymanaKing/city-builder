bfd16223b2c8d7435756c712a6a6a7f21be9110d
Khayman King <khaymanak@gmail.com>
2022-03-10 16:25:52 -0800

    grabbed the dom

--- app.js
+++ app.js
@@ -1,5 +1,15 @@
 // import functions and grab DOM elements
+const lakeDropDown = document.getElementById('lake-dropdown');
+const forestDropDown = document.getElementById('forest-dropdown');
+const treehouseDropDown = document.getElementById('treehouse-dropdown');
 
+const lakeEl  = document.getElementById('lake-image');
+const forestEl  = document.getElementById('forest-image');
+const treehouseEl  = document.getElementById('treehouse-image');
+
+const sloganEl = document.getElementById('slogans');
+const sloganInputEl = document.getElementById('slogans-input');
+const sloganButtonEl = document.getElementById('slogan-button');
 // let state
 
 // set event listeners 
