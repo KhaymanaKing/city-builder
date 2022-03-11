27b2c36d1f2f8959359db23334ed56359d3aef17
Khayman King <khaymanak@gmail.com>
2022-03-11 10:18:41 -0800

    now counts dropdown changes

--- app.js
+++ app.js
@@ -3,14 +3,16 @@ const lakeDropDown = document.getElementById('lake-dropdown');
 const forestDropDown = document.getElementById('forest-dropdown');
 const treehouseDropDown = document.getElementById('treehouse-dropdown');
 
-const lakeEl  = document.getElementById('lake-image');
-const forestEl  = document.getElementById('forest-image');
-const treehouseEl  = document.getElementById('treehouse-image');
+const lakeEl = document.getElementById('lake-image');
+const forestEl = document.getElementById('forest-image');
+const treehouseEl = document.getElementById('treehouse-image');
+
 // need to add Stats dom for tracking everything that I do 
 
 const sloganEl = document.getElementById('slogans');
 const sloganInputEl = document.getElementById('slogans-input');
 const sloganButtonEl = document.getElementById('slogan-button');
+const sloganList = document.getElementById('report');
 // let state
 
 let lakeCount = 0;
@@ -41,32 +43,41 @@ treehouseDropDown.addEventListener('change', () => {
     displayStats();
 });
 
-sloganButtonEl.addEventListener('click', () =>{
+sloganButtonEl.addEventListener('click', ()=>{
+    
     const newSlogan = sloganInputEl.value;
 
-    slogans.push(newSlogan);
+    sloganEl.push(newSlogan);
+    
     displaySlogan();
     sloganInputEl.value = '';
 });
+
   // get user input
 
   //need to fix displaySlogan function so it actually works. I think my syntax may be wrong? 
 
 
-function displaySlogan(){
+function displaySlogan() {
+    
     sloganEl.textContent = '';
-    for (let slogan of slogans){
-
-        const pTag = document.createElement('p');
         
-        pTag.classList.add('slogans');
+    for (let slogan of slogans) {
+        
+        const pTag = document.createElement('p');
+    
         pTag.textContent = slogan;
-        sloganEl.append(pTag); 
-        console.log(sloganEl);
+        pTag.classList.add('sloganList');
+        
+        sloganEl.append(pTag);
+        
     }
 }
+    
 
-//need to add displaySlogan function for everytime I change my dropdown it counts 
-function displaystat(){
-  slo
+
+function displayStats() {
+    sloganList.textContent = `You have changed the lake ${lakeCount} times.
+    You have changed the forest ${forestCount} times.
+    You have changed the tree house ${treehouseCount} times.`;
 }
